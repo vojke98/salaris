e826f6033453982f49b0e43ba29764492d13d143
vojke98 <dejanvojinovic@yahoo.com>
2021-11-09 15:21:26 +0100

    refactor: Improve initialize.py script

--- backend/create_super_user.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@salaris.com', '#tpo2021')" | python manage.py shell
